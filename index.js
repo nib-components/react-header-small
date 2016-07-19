9f7e38bb98ec9b3a434f9dfc29e5f6ed4f82297f
James Newell <j.newell@nib.com.au>
2016-07-19 15:35:01 +1000

    fix deps and clean up files which should be ignored

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ export default class HeaderSmall extends React.Component {
   render() {
     return (
       <div className="micro-header">
-        <a className="micro-header__logo" href="/" id="ga-inlink-logo-homepage" title="nib Private Health Insurance">nib Private Health Insurance</a>
+        <a className="micro-header__logo" href="/" id="ga-inlink-logo-homepage" title="nib Private Health Insurance">
+          nib Private Health Insurance
+        </a>
       </div>
     );
   }
